a77a5e78e94319db118c019435affab0d393346f
Anwar Muthohari <anwar.muthohari@gmail.com>
2020-03-07 17:22:14 +0700

    [V.03][commands.php] - init load AddCommandMultiply

--- commands.php
+++ commands.php
@@ -3,4 +3,5 @@
 return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\AddCommandSubtract::class,
+    \Jakmall\Recruitment\Calculator\Commands\AddCommandMultiply::class,
 ];
