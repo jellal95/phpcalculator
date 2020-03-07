830e1c3a81434c5e88b61b03d7bce5edcb27af35
Anwar Muthohari <anwar.muthohari@gmail.com>
2020-03-07 17:30:28 +0700

    [V.04][commands.php] - init load AddCommandDivide

--- commands.php
+++ commands.php
@@ -4,4 +4,5 @@ return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\AddCommandSubtract::class,
     \Jakmall\Recruitment\Calculator\Commands\AddCommandMultiply::class,
+    \Jakmall\Recruitment\Calculator\Commands\AddCommandDivide::class,
 ];
